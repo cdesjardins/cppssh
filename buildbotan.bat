16d28464052823214f8c81fc7b342e4b1cd7451d
Chris Desjardins <git@chrisd.info>
2015-06-09 00:16:33 +0200

    Cleanup botan before building it

--- buildbotan.bat
+++ buildbotan.bat
@@ -1,8 +1,10 @@
 pushd .
 cd ..\botan
+del /s /q build botan_all*.* botan*.lib botan*.exe
 configure.py --disable-shared --disable-modules=selftest,tls --build-mode=release --cpu=i386 --via-amalgamation --maintainer-mode --prefix=%USERPROFILE%\software_devel\repo\install
 sed -i 's/cl \/MD/cl \/MT/' Makefile
 nmake install
+del /s /q build botan_all*.* botan*.lib botan*.exe
 configure.py --disable-shared --disable-modules=selftest,tls --build-mode=debug   --cpu=i386 --via-amalgamation --maintainer-mode --prefix=%USERPROFILE%\software_devel\repo\install --libdir=%USERPROFILE%\software_devel\repo\install\lib\Debug
 sed -i 's/cl \/MD/cl \/MT/' Makefile
 nmake install
