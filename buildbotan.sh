40832192b33b8689f3c99b4d41641a8030488206
Chris Desjardins <git@chrisd.info>
2015-06-08 23:32:45 +0200

    Add shell

--- buildbotan.sh
+++ buildbotan.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 pushd .
 cd ../botan
 ./configure.py --disable-shared --disable-modules=selftest,tls --prefix=$HOME/sw/repo/install --libdir=$HOME/sw/repo/install/lib/Debug --build-mode=debug --via-amalgamation --disable-avx2 --maintainer-mode
