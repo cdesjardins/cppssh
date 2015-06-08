10b301a13d77f06a326a137c32e7a03efa5d714e
Chris Desjardins <git@chrisd.info>
2015-06-09 00:17:28 +0200

    Clean botan on the linux side

--- buildbotan.sh
+++ buildbotan.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 pushd .
 cd ../botan
+rm -rf build botan_all*.* botan*.a
 ./configure.py --disable-shared --disable-modules=selftest,tls --prefix=$HOME/sw/repo/install --libdir=$HOME/sw/repo/install/lib/Debug --build-mode=debug --via-amalgamation --disable-avx2 --maintainer-mode
 make -j8 install
+rm -rf build botan_all*.* botan*.a
 ./configure.py --disable-shared --disable-modules=selftest,tls --prefix=$HOME/sw/repo/install --build-mode=release --via-amalgamation --disable-avx2 --maintainer-mode
 make -j8 install
 popd .
