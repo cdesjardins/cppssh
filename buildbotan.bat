c8d2f44e8d6601e346ce3caff1e81a9e4eb34410
Chris Desjardins <git@chrisd.info>
2015-06-09 00:05:42 +0200

    Update CDLogger linking and botan building

--- buildbotan.bat
+++ buildbotan.bat
@@ -6,4 +6,4 @@ nmake install
 configure.py --disable-shared --disable-modules=selftest,tls --build-mode=debug   --cpu=i386 --via-amalgamation --maintainer-mode --prefix=%USERPROFILE%\software_devel\repo\install --libdir=%USERPROFILE%\software_devel\repo\install\lib\Debug
 sed -i 's/cl \/MD/cl \/MT/' Makefile
 nmake install
-popd .
+popd
