db93fedd0810de8b476a9af1684fd40a43710157
Chris Desjardins <git@chrisd.info>
2015-06-12 00:35:37 +0200

    Revert botan library name change

--- buildbotan.sh
+++ buildbotan.sh
@@ -2,9 +2,9 @@
 pushd .
 cd ../botan
 rm -rf build botan_all*.* libbotan*.a
-./configure.py --disable-shared --disable-modules=selftest,tls --prefix=$HOME/sw/repo/install --libdir=$HOME/sw/repo/install/lib/Debug --build-mode=debug --via-amalgamation --disable-avx2 --maintainer-mode
+./configure.py --disable-shared --disable-modules=tls --prefix=$HOME/sw/repo/install --libdir=$HOME/sw/repo/install/lib/botan/Debug --build-mode=debug --via-amalgamation --disable-avx2 --maintainer-mode
 make -j8 install
 rm -rf build botan_all*.* libbotan*.a
-./configure.py --disable-shared --disable-modules=selftest,tls --prefix=$HOME/sw/repo/install --build-mode=release --via-amalgamation --disable-avx2 --maintainer-mode
+./configure.py --disable-shared --disable-modules=tls --prefix=$HOME/sw/repo/install --libdir=$HOME/sw/repo/install/lib/botan/Release --build-mode=release --via-amalgamation --disable-avx2 --maintainer-mode
 make -j8 install
 popd .
