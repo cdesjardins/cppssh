2fa72a5afa2720fa770ed879a0fce682069f6732
Chris Desjardins <git@chrisd.info>
2015-06-13 13:59:43 +0200

    Remove unnecessary makefile cleanup crud

--- buildbotan.bat
+++ buildbotan.bat
@@ -3,11 +3,9 @@ cd ..\botan
 del /s /q build botan_all*.* botan*.lib botan*.exe *.dll
 configure.py --disable-shared --disable-modules=tls --build-mode=release --cpu=i386 --via-amalgamation --maintainer-mode --prefix=%USERPROFILE%\software_devel\repo\install --libdir=%USERPROFILE%\software_devel\repo\install\lib\botan\Release
 sed -i 's/cl \/MD/cl \/MT/' Makefile
-sed -i 's/LIBNAME       = botan//' Makefile
 nmake install
 del /s /q build botan_all*.* botan*.lib botan*.exe *.dll
 configure.py --disable-shared --disable-modules=tls --build-mode=debug   --cpu=i386 --via-amalgamation --maintainer-mode --prefix=%USERPROFILE%\software_devel\repo\install --libdir=%USERPROFILE%\software_devel\repo\install\lib\botan\Debug
 sed -i 's/cl \/MD/cl \/MT/' Makefile
-sed -i 's/LIBNAME       = botan//' Makefile
 nmake install
 popd
