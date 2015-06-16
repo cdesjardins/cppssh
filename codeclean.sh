43a3cdc961e17b54503389604febb10d30ab1a8a
Chris Desjardins <git@chrisd.info>
2015-06-16 23:18:00 +0200

    Update to new version of cppcheck

--- codeclean.sh
+++ codeclean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-~/Downloads/cppcheck-1.68/cppcheck -Iinclude/ -I../botan/install/include/botan-1.11/ -I../CDLogger/include/ --std=c++11 --enable=all --force  . 2> err.txt
+~/Downloads/cppcheck-1.69/cppcheck -Iinclude/ -I../botan/install/include/botan-1.11/ -I../CDLogger/include/ --std=c++11 --enable=all --force  . 2> err.txt
 call_Uncrustify.sh . cpp
 call_Uncrustify.sh . h
 cat err.txt
