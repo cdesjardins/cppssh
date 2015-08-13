4b669371e0341dbd09686d8d02f6c6c3f482711c
Christopher Desjardins <christopher.desjardins@tomtom.com>
2015-08-13 23:41:34 +0800

    Fix popd

--- buildbotan.sh
+++ buildbotan.sh
@@ -7,4 +7,4 @@ make -j8 install
 rm -rf build botan_all*.* libbotan*.a
 ./configure.py --disable-shared --disable-modules=tls --prefix=$HOME/sw/repo/install --libdir=$HOME/sw/repo/install/lib/botan/Release --build-mode=release --via-amalgamation --disable-avx2 --maintainer-mode
 make -j8 install
-popd .
+popd
