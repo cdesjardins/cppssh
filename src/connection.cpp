988693bc6e76e1855c9f46ae36825459696f5923
Chris Desjardins <git@chrisd.info>
2015-09-19 14:30:25 +0200

    Fix auth with encrypted private key

--- src/connection.cpp
+++ src/connection.cpp
@@ -89,7 +89,7 @@ bool CppsshConnection::connect(const char* host, const short port, const char* u
     {
         return false;
     }
-    if ((authWithPassword(username, password) == false) && (authWithKey(username, privKeyFile, password) == false))
+    if ((authWithKey(username, privKeyFile, password) == false) && (authWithPassword(username, password) == false))
     {
         return false;
     }
