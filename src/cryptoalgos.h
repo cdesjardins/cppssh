32a42dec43170e51ce15bd0002e8702857660005
Chris Desjardins <git@chrisd.info>
2016-01-08 08:20:03 -0800

    Fix compiler error with vs2015

--- src/cryptoalgos.h
+++ src/cryptoalgos.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iterator>
 #include "CDLogger/Logger.h"
 #include "strtrim.h"
 
